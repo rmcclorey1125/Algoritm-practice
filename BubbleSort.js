7582197ba0ddb9a09262c1a77ad9dac1214b2590
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-12-08 14:51:25 -0500

    Start Sort

---
+++ BubbleSort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+    let noSwaps
+    for(let i = arr.length; i > 0; i--){
+        for(let j = 0; j < i - 1; j++){
+            let temp = arr[j]
+            if(arr[j] > arr[j+1]){
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                noSwaps = false
+            }
+        }
+    }
+    if (noSwaps) break
+} 
