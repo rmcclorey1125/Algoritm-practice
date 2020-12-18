c79e391fcc7a02b28faea3fefd25d98c6cdbad83
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-12-17 20:05:07 -0500

    Doubly Linked List

---
+++ DoublyLinkedList.js
@@ -0,0 +1,147 @@
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null
+        this.prev = null
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    push(val){
+        let newNode = new Node(val)
+        if(this.length === 0){
+            this.head = newNode
+            this.tail = newNode
+        }else{
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+
+    pop(){
+        if(this.length === 0){
+            return undefined
+        }
+        let oldTail = this.tail
+        if(this.length === 1){
+            this.head = null
+            this.tail = null
+        } else {
+            this.tail = oldTail.prev
+            this.tail.next = null
+            oldTail.prev = null
+        }
+        this.length--
+        return oldTail
+    }
+
+    shift(){
+        if(this.length === 0){
+            return undefined
+        }
+        let oldHead = this.head
+        if(this.length === 1){
+            this.head === null
+            this.tail === null
+        }
+        this.head = oldHead.next
+        this.head.prev = null
+        oldHead.next = null
+        this.length--
+        return oldHead
+    }
+
+    unshift(val){
+        let newNode = new Node(val)
+        if(this.length === 0){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+
+    get(index){
+        if(index < 0 || index >= this.length){
+            return null
+        }
+        if(index <= this.length / 2){
+            let count = 0
+            let current = this.head
+            while(count !== index){
+                current = current.next
+                count++
+            }
+        } else {
+            let count = this.length - 1
+            let current = this.tail
+            while(count !== index){
+                current = current.prev
+                count--
+            }
+        }
+        return current
+    }
+
+    set(index, val){
+        let findNode = this.get(index)
+        if(!findNode){
+            return false
+        }
+        findNode.val = val
+        return true
+    }
+
+    insert(index, val){
+        if(index < 0 || index > this.length){
+            return false
+        }
+        if(index === 0){
+            return !!this.unshift(val)
+        }
+        if(index === this.length){
+            return !!this.push(val)
+        }
+        let newNode = new Node(val)
+        let beforeNode = this.get(index - 1)
+        let afterNode = beforeNode.next
+        beforeNode.next = newNode
+        newNode.prev = beforeNode
+        newNode.next = afterNode
+        afterNode.prev = newNode
+        this.length++
+        return true
+    }
+
+    remove(index){
+        if(index < 0 || index >= this.length){
+            return undefined
+        }
+        if(index === 0){
+            return this.shift()
+        }
+        if(index === this.length - 1){
+            return this.pop()
+        }
+        let removeNode = this.get(index)
+        removeNode.prev.next = removeNode.next
+        removeNode.next.prev = removeNode.prev
+        removeNode.next = null
+        removeNode.prev = null
+        this.length--
+        return removedNode
+    }
+}
