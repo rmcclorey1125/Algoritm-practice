13ae18c87c587ff8b364f661e92ed55a846ac130
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-12-29 17:36:50 -0500

    Dijkstras Algorithm

--- DijkstrasAlgo.js
+++ DijkstrasAlgo.js
@@ -1,3 +1,63 @@
+class WeightedGraph {
+    constructor() {
+        this.adjacencyList = {}
+    }
+
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]){ 
+            this.adjacencyList[vertex] = []
+        }
+    }
+
+    addEdge(vertex1, vertex2, weight){
+        this.adjacencyList[vertex1].push({node:vertex2,weight})
+        this.adjacencyList[vertex2].push({node:vertex1,weight})
+    }
+
+    Dijkstra(start, finish){
+        const nodes = new PriorityQueue()
+        const distances = {}
+        const previous = {}
+        let path = []
+        let smallest
+
+        for(let vertex in this.adjacencyList){
+            if(vertex === start){
+                distances[vertex] = 0
+                nodes.enqueue(vertex, 0)
+            } else {
+                distances[vertex] = Infinity
+                nodes.enqueue(vertex, Infinity)
+            }
+            previous[vertex] = null
+        }
+
+        while(nodes.value.length){
+            smallest = nodes.dequeue().val
+            if(smallest === finish){
+                while(previous[smallest]){
+                    path.push(smallest)
+                    smallest = previous[smallest]
+                }
+                break
+            }
+            if(smallest || distances[smallest] !== Infinity){
+                for(let neighbor in this.adjacencyList[smallest]){
+                    let nextNode = this.adjacencyList[smallest][neighbor]
+                    let candidate = distances[smallest] + nextNode.weight
+                    let nextNeighbor = nextNode.node
+                    if(candidate < distances[nextNeighbor]){
+                        distances[nextNeighbor] = candidate
+                        previous[nextNeighbor] = smallest
+                        nodes.enqueue(nextNeighbor, candidate)
+                    }
+                }
+            }
+        }
+        return path.concat(smallest).reverse()
+    }
+}
+
 class priorityQueue{
     constructor(){
         this.values = []
@@ -71,4 +131,3 @@ class node{
         this.priority = priority
     }
 }
-
