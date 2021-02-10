a0c7fa4f8d403c5d0fa80ca5ab6b42fa4b6bd232
rmcclorey1125 <rmcclorey1@yahoo.com>
2021-02-09 20:45:04 -0500

    leetcode

---
+++ SearchInsert.js
@@ -0,0 +1,10 @@
+const searchInsert = (nums, target) => {
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] >= target){
+            return i
+        }
+    }
+    return nums.length
+};
+
+searchInsert([1,3,5,6], 5)
---
+++ strStr.js
@@ -0,0 +1,13 @@
+const strStr = (haystack, needle) => {
+    if(needle.length === 0){
+        return 0
+    }
+    for(let i = 0; i < haystack.length; i++){
+        if(haystack.substr(i, needle.length) === needle){
+            return i
+        }
+    }
+    return -1
+};
+
+strStr("hello", "ll")
