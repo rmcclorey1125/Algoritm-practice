bf6582389fd50dc6f4ca0aaf915b96a6ee705dde
rmcclorey1125 <rmcclorey1@yahoo.com>
2021-02-11 15:59:07 -0500

    smallest

---
+++ Smallest.js
@@ -0,0 +1,22 @@
+const arr = [1,3,6,4,1,2]
+
+const findSmallest = (a) => {
+    const set = new Set()
+    let max = 0
+    for(let i = 0; i < a.length; i++){
+        if(a[i] > 0){
+            set.add(a[i])
+            max = Math.max(max, a[i])
+        }
+    }
+
+    for(let i = 1; i < max; i++){
+        if(!set.has(i)){
+            console.log('ans', i)
+            return i
+        }
+    }
+    return max+1
+}
+
+findSmallest(arr)
