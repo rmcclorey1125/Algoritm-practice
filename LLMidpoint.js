41cb4f436bf093030ec64b79dbdfc1a2988e851b
rmcclorey1125 <rmcclorey1@yahoo.com>
2021-02-27 13:41:16 -0500

    Linked Lists

---
+++ LLMidpoint.js
@@ -0,0 +1,9 @@
+function midpoint(list) {
+    let slow = list.head
+    let fast = list.head
+    while(fast.next && fast.next.next){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    return slow
+}
